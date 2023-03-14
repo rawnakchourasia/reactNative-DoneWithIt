1dd3d231d44d61278f8a6c1be996cb78c54c84e0
Rawnak Chourasia <rnkchr0@gmail.com>
2023-03-14 22:59:31 +0530

    Text and Image

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 
 export default function App() {
-  console.log("App started!");
+  const handlePress = () => {
+    console.log("Pressed!!!");
+    console.log(require("./assets/favicon.png"));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome to React Native!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Welcome to React Native! loop lplp m h hhjjhh ggt ggggu u u i i ioooopp
+        g tt f c yg gh gh v g gf gg h hgh gh gy ft v g fgf h{" "}
+      </Text>
+      <Image source={require("./assets/favicon.png")} />
+      <Image
+        blurRadius={10}
+        fadeDuration={1000} // in millisec, only works on android
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +33,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    // paddingTop: 30,
     justifyContent: "center",
+    alignItems: "center",
   },
 });
