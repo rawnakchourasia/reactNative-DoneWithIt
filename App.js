cfef14d2d8c666edbe2f99d4ffc66295e86057d8
Rawnak Chourasia <rnkchr0@gmail.com>
2023-03-26 00:33:18 +0530

    Building the welcome screen

--- App.js
+++ App.js
@@ -19,49 +19,13 @@ import {
 
 import { useDeviceOrientation } from "@react-native-community/hooks";
 import { useWindowDimensions } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   const orientation = useDeviceOrientation();
   console.log(useWindowDimensions());
 
-  return (
-    <View
-      style={{
-        backgroundColor: "#fff",
-        flex: 1,
-        flexDirection: "row", // This can also be row-reverse, column and column-reverse
-        justifyContent: "center", // Works like in css
-        alignItems: "center", //The default value is stretch, and can be changed to baseline, flex-end
-        alignContent: "center", // This is only used when flexWrap is used
-        flexWrap: "wrap",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          top: 20,
-          left: 20,
-          position: "absolute", // By default the position is relative
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 100,
-        }}
-      ></View>
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      ></View>
-    </View>
-  );
+  return <WelcomeScreen />;
 }
 
 const styles = StyleSheet.create({
