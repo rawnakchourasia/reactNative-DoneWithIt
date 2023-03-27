c6fec65da290323c1569c1f8792aaa1415af33b4
Rawnak Chourasia <rnkchr0@gmail.com>
2023-03-27 13:10:22 +0530

    Button customised - AppButton created

--- App.js
+++ App.js
@@ -1,36 +1,8 @@
-import { StatusBar as StatusBar1 } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableWithoutFeedback,
-  TouchableHighlight,
-  TouchableOpacity,
-  TouchableNativeFeedback,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-  Dimensions,
-} from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";
-
-import { useDeviceOrientation } from "@react-native-community/hooks";
-import { useWindowDimensions } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-
-import AppText from "./app/components/AppText";
+import { StyleSheet, View, Platform, StatusBar } from "react-native";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
-  const orientation = useDeviceOrientation();
-  console.log(useWindowDimensions());
-
-  // return <WelcomeScreen />;
-  // return <ViewImageScreen />;
   return (
     <View
       style={{
@@ -39,8 +11,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <MaterialIcons name="email" size={50} color="dodgerblue" />
-      <AppText>This is React Native!</AppText>
+      <AppButton title={"Login"} onPress={() => console.log("Tapped")} />
     </View>
   );
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -7,17 +7,8 @@ const AppText = (props) => {
 
 const styles = StyleSheet.create({
   text: {
-    color: "tomato",
-    ...Platform.select({
-      ios: {
-        fontSize: 20,
-        fontFamily: "Avenir",
-      },
-      android: {
-        fontSize: 18,
-        fontFamily: "Roboto",
-      },
-    }),
+    fontSize: 18,
+    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
   },
 });
 
