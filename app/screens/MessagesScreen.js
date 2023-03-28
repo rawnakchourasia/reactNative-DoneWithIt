13e65fbd8c5b685d4f7cf81a9d4828502d79011f
Rawnak Chourasia <rnkchr0@gmail.com>
2023-03-28 22:11:41 +0530

    MessagesScreen component

---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { FlatList } from "react-native";
+import ListItem from "../components/ListItem";
+
+const messages = [
+  {
+    id: 1,
+    title: "T1",
+    description: "D1",
+    image: require("../assets/rawnak.jpg"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/rawnak.jpg"),
+  },
+  {
+    id: 3,
+    title: "T3",
+    description: "D3",
+    image: require("../assets/rawnak.jpg"),
+  },
+];
+
+const MessagesScreen = (props) => {
+  return (
+    <FlatList
+      data={messages}
+      keyExtractor={(message) => message.id.toString()}
+      renderItem={({ item }) => (
+        <ListItem
+          title={item.title}
+          subTitle={item.description}
+          image={item.image}
+        />
+      )}
+    />
+  );
+};
+
+export default MessagesScreen;
