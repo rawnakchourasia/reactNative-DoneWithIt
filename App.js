9fdcdfd7328c06041573fe5da727a953cbdea742
Rawnak Chourasia <rnkchr0@gmail.com>
2023-03-28 17:46:47 +0530

    ViewImageScreen Styling

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 import colors from "./app/config/colors";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import colors from "../config/colors";
 
 const ViewImageScreen = (props) => {
   return (
     <View style={styles.container}>
-      <View style={styles.closeIcon}></View>
-      <View style={styles.deleteIcon}></View>
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" color={colors.white} size={30} />
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          color={colors.white}
+          size={35}
+        />
+      </View>
       <Image
         resizeMode="contain"
         style={styles.image}
@@ -19,21 +28,15 @@ const ViewImageScreen = (props) => {
 
 const styles = StyleSheet.create({
   closeIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
-    left: 30,
+    left: 35,
   },
   container: {
     backgroundColor: colors.black,
     flex: 1,
   },
   deleteIcon: {
-    width: 50,
-    height: 50,
-    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,
