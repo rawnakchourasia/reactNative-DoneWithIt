bc01ae4ff74935e1f554ab1173bd64738dca4815
Rawnak Chourasia <rnkchr0@gmail.com>
2023-03-28 16:27:06 +0530

    Making card component

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { StyleSheet, View, Platform, StatusBar } from "react-native";
+import { StyleSheet, View, Platform, StatusBar, Text } from "react-native";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
+import colors from "./app/config/colors";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <View
+      style={{
+        backgroundColor: "#f8f4f4",
+        padding: 20,
+        paddingTop: 100,
+      }}
+    >
+      <Card
+        title="Red Jacket for Sale"
+        subTitle="$100"
+        image={require("./app/assets/sale_jacket.jpg")}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Text, StyleSheet, Platform } from "react-native";
 
-const AppText = (props) => {
-  return <Text style={styles.text}>{props.children}</Text>;
+const AppText = ({ children, style }) => {
+  return <Text style={[styles.text, style]}>{children}</Text>;
 };
 
 const styles = StyleSheet.create({
