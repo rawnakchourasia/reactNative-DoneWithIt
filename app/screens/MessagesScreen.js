63cd2b098ef0177315bbd27e1cb6ca1af381b125
Rawnak Chourasia <rnkchr0@gmail.com>
2023-03-28 22:58:53 +0530

    Screen Component | Expo Constants installation

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 import ListItem from "../components/ListItem";
+
+import Screen from "../components/Screen";
 
 const messages = [
   {
@@ -25,18 +27,22 @@ const messages = [
 
 const MessagesScreen = (props) => {
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={(message) => message.id.toString()}
-      renderItem={({ item }) => (
-        <ListItem
-          title={item.title}
-          subTitle={item.description}
-          image={item.image}
-        />
-      )}
-    />
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
   );
 };
+
+const styles = StyleSheet.create({});
 
 export default MessagesScreen;
