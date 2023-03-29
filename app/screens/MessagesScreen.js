09e26e49c4ba1583e8a979da4a670eb900f11c8e
Rawnak Chourasia <rnkchr0@gmail.com>
2023-03-29 17:00:13 +0530

    pull to refresh list

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -29,6 +29,7 @@ const initialMessages = [
 
 const MessagesScreen = (props) => {
   const [messages, setMessages] = useState(initialMessages);
+  const [refreshing, setRefreshing] = useState(false);
 
   const handleDelete = (message) => {
     setMessages(messages.filter((m) => m.id !== message.id));
@@ -51,6 +52,23 @@ const MessagesScreen = (props) => {
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={() => {
+          setMessages([
+            {
+              id: 1,
+              title: "T1",
+              description: "D1",
+              image: require("../assets/rawnak.jpg"),
+            },
+            {
+              id: 2,
+              title: "T2",
+              description: "D2",
+              image: require("../assets/rawnak.jpg"),
+            },
+          ]);
+        }}
       />
     </Screen>
   );
