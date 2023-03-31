718c881aeeb33ca6470e00fa72b0ad37578e8cde
Rawnak Chourasia <rnkchr0@gmail.com>
2023-04-01 01:50:41 +0530

    Picker with useRef and useState

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Switch } from "react-native";
 import { TextInput, StyleSheet } from "react-native";
 import AppTextInput from "./app/components/AppTextInput";
@@ -12,11 +12,23 @@ import { Picker } from "@react-native-picker/picker";
 
 export default function App() {
   const [selectedLanguage, setSelectedLanguage] = useState();
+  const pickerRef = useRef();
+
+  function open() {
+    pickerRef.current.focus();
+  }
+
+  function close() {
+    pickerRef.current.blur();
+  }
+
   // return <ListingsScreen />;
   return (
     <Screen>
       <Picker
+        ref={pickerRef}
         selectedValue={selectedLanguage}
+        selectionColor="black"
         onValueChange={(itemValue, itemIndex) => setSelectedLanguage(itemValue)}
       >
         <Picker.Item label="Java" value="java" />
