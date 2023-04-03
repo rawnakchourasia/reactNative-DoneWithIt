774b618218f106f44c9dbcdd8d5730b3b706271f
Rawnak Chourasia <rnkchr0@gmail.com>
2023-04-03 23:59:43 +0530

    Some Styles

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -5,7 +5,7 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 const Screen = ({ children, style }) => {
   return (
     <SafeAreaView style={[styles.screen, style]}>
-      <View>{children}</View>
+      <View style={[styles.view, style]}>{children}</View>
     </SafeAreaView>
   );
 };
@@ -13,6 +13,9 @@ const Screen = ({ children, style }) => {
 const styles = StyleSheet.create({
   screen: {
     paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+  view: {
     flex: 1,
   },
 });
